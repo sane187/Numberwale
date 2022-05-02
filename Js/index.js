d62c1eff1621e12e3154f2da072e68a68db2956d
Arpit joshi <joshiarpit75@gmail.com>
2022-05-02 16:30:27 +0530

    commit last one

--- Js/index.js
+++ Js/index.js
@@ -16,3 +16,13 @@ const toggleMenu =()=>{
 }
 
 menuBtn.addEventListener("click", toggleMenu);
+
+// document.onreadystatechange = function() {
+// 	if (document.readyState !== "complete") {
+// 		document.querySelector("body").style.visibility = "hidden";
+// 		document.querySelector("#loader").style.visibility = "visible";
+// 	} else {
+// 		document.querySelector("#loader").style.display = "none";
+// 		document.querySelector("body").style.visibility = "visible";
+// 	}
+// };
