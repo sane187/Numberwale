ff4318d1a8e691504836ef1729184bc201b95744
Arpit joshi <joshiarpit75@gmail.com>
2022-05-06 16:20:34 +0530

    commit

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,7 @@
 let menuButton=document.querySelector(".nav-mob-toggler");
 let overlayDiv=document.querySelector(".overlay-content");
+let categoryCont=document.querySelector(".categories-cont");
+
 let showMenu = false;
 
 const toggleMenu =()=>{
@@ -9,7 +11,7 @@ const toggleMenu =()=>{
     
         menuBtn.classList.remove("fa-bars");
         menuBtn.classList.add("fa-xmark");
-        overlayDiv.style.width="100%";
+        overlayDiv.style.width="100vw";
         overlayDiv.style.height="100%";
        showMenu =true;
     }
@@ -24,6 +26,13 @@ const toggleMenu =()=>{
   
 }
 
+
+const aj =()=>{
+    console.log("scroll")
+    window.scrollTo(500, 0);
+}
+
+overlayDiv.addEventListener("scroll",aj)
 menuButton.addEventListener("click", toggleMenu);
 
 // document.onreadystatechange = function() {
@@ -35,4 +44,26 @@ menuButton.addEventListener("click", toggleMenu);
 // 		document.querySelector("body").style.visibility = "visible";
 // 	}
 // };
+const arr1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+categoryCont.innerHTML= arr1.forEach(element =>{
+    `<div>UNIQUE Numbers</div>`
+})
 
+
+function animateValue(id, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 20 : -20;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+animateValue("value", 1000, 10000, 10000);
