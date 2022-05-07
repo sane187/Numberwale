a1f73a3780e7c80fd6de3dfc0d487980b09ef37c
Arpit joshi <joshiarpit75@gmail.com>
2022-05-07 13:57:30 +0530

    comm

--- Js/index.js
+++ Js/index.js
@@ -3,7 +3,20 @@ let overlayDiv=document.querySelector(".overlay-content");
 let categoryCont=document.querySelector(".categories-cont");
 
 let showMenu = false;
-
+window.addEventListener("scroll",function(){
+    let nav=document.getElementById("navbar-top");
+    if(window.pageYOffset==0){
+     
+      nav.classList.remove("fixed-top");
+     
+    }
+    if(window.pageYOffset>0){
+     
+      nav.classList.add("fixed-top");
+    
+     }
+  })
+  
 const toggleMenu =()=>{
     let menuBtn=document.getElementById("nav-mob-icon");
 
@@ -26,14 +39,14 @@ const toggleMenu =()=>{
   
 }
 
-
-const aj =()=>{
-    console.log("scroll")
-    window.scrollTo(500, 0);
-}
-
-overlayDiv.addEventListener("scroll",aj)
 menuButton.addEventListener("click", toggleMenu);
+// const aj =()=>{
+//     console.log("scroll")
+//     window.scrollTo(500, 0);
+// }
+
+// overlayDiv.addEventListener("scroll",aj)
+
 
 // document.onreadystatechange = function() {
 // 	if (document.readyState !== "complete") {
@@ -44,26 +57,31 @@ menuButton.addEventListener("click", toggleMenu);
 // 		document.querySelector("body").style.visibility = "visible";
 // 	}
 // };
-const arr1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-categoryCont.innerHTML= arr1.forEach(element =>{
-    `<div>UNIQUE Numbers</div>`
-})
 
 
-function animateValue(id, start, end, duration) {
-    if (start === end) return;
-    var range = end - start;
-    var current = start;
-    var increment = end > start? 20 : -20;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var obj = document.getElementById(id);
-    var timer = setInterval(function() {
-        current += increment;
-        obj.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
+// const arr1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+// arr1.forEach(element =>{
+//     const carouselItem =document.createElement("div");
+//     carouselItem.innerText="UNIQUE Numbers"
+//     carouselItem.classList.add("category-name")
+//     // categoryCont.appendChild(carouselItem)
+// })
 
-animateValue("value", 1000, 10000, 10000);
+
+// function animateValue(id, start, end, duration) {
+//     if (start === end) return;
+//     var range = end - start;
+//     var current = start;
+//     var increment = end > start? 20 : -20;
+//     var stepTime = Math.abs(Math.floor(duration / range));
+//     var obj = document.getElementById(id);
+//     var timer = setInterval(function() {
+//         current += increment;
+//         obj.innerHTML = current;
+//         if (current == end) {
+//             clearInterval(timer);
+//         }
+//     }, stepTime);
+// }
+
+// animateValue("value", 1000, 10000, 10000);
