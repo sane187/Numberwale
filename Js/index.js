d276a309fcd625be6defe730ae274bc33dee857b
Arpit joshi <joshiarpit75@gmail.com>
2022-05-22 20:58:33 +0530

    comm-main

--- Js/index.js
+++ Js/index.js
@@ -460,30 +460,44 @@ Total -<strong class="mx-2">37</strong> | Sum - <strong class="mx-2">8</strong> 
   })
 })
 
-function getVals(){
-  // Get slider values
-  var parent = this.parentNode;
-  var slides = parent.getElementsByTagName("input");
-    var slide1 = parseFloat( slides[0].value );
-    var slide2 = parseFloat( slides[1].value );
-  // Neither slider will clip the other, so make sure we determine which is larger
-  if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
-  
-  var displayElement = parent.getElementsByClassName("rangeValues")[0];
-      displayElement.innerHTML = "$ " + slide1 + "k - $" + slide2 + "k";
-}
+(function() {
 
-window.onload = function(){
-  // Initialize Sliders
-  var sliderSections = document.getElementsByClassName("range-slider");
-      for( var x = 0; x < sliderSections.length; x++ ){
-        var sliders = sliderSections[x].getElementsByTagName("input");
-        for( var y = 0; y < sliders.length; y++ ){
-          if( sliders[y].type ==="range" ){
-            sliders[y].oninput = getVals;
-            // Manually trigger event first time to display values
-            sliders[y].oninput();
-          }
-        }
+  function addSeparator(nStr) {
+      nStr += '';
+      var x = nStr.split('.');
+      var x1 = x[0];
+      var x2 = x.length > 1 ? '.' + x[1] : '';
+      var rgx = /(\d+)(\d{3})/;
+      while (rgx.test(x1)) {
+          x1 = x1.replace(rgx, '$1' + '.' + '$2');
       }
-}
+      return x1 + x2;
+  }
+
+  function rangeInputChangeEventHandler(e){
+      var rangeGroup = $(this).attr('name'),
+          minBtn = $(this).parent().children('.min'),
+          maxBtn = $(this).parent().children('.max'),
+          range_min = $(this).parent().children('.range_min'),
+          range_max = $(this).parent().children('.range_max'),
+          minVal = parseInt($(minBtn).val()),
+          maxVal = parseInt($(maxBtn).val()),
+          origin = $(this).context.className;
+
+      if(origin === 'min' && minVal > maxVal-5){
+          $(minBtn).val(maxVal-5);
+      }
+      var minVal = parseInt($(minBtn).val());
+      $(range_min).html(addSeparator(minVal*1000) + ' €');
+
+
+      if(origin === 'max' && maxVal-5 < minVal){
+          $(maxBtn).val(5+ minVal);
+      }
+      var maxVal = parseInt($(maxBtn).val());
+      $(range_max).html(addSeparator(maxVal*1000) + ' €');
+  }
+
+$('input[type="range"]').on( 'input', rangeInputChangeEventHandler);
+})();
+
