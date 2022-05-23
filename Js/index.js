3b229c1d0f157b39a0a1534519f9db112f02bae9
Arpit joshi <joshiarpit75@gmail.com>
2022-05-23 10:22:14 +0530

    comm

--- Js/index.js
+++ Js/index.js
@@ -460,44 +460,21 @@ Total -<strong class="mx-2">37</strong> | Sum - <strong class="mx-2">8</strong> 
   })
 })
 
-(function() {
-
-  function addSeparator(nStr) {
-      nStr += '';
-      var x = nStr.split('.');
-      var x1 = x[0];
-      var x2 = x.length > 1 ? '.' + x[1] : '';
-      var rgx = /(\d+)(\d{3})/;
-      while (rgx.test(x1)) {
-          x1 = x1.replace(rgx, '$1' + '.' + '$2');
-      }
-      return x1 + x2;
-  }
-
-  function rangeInputChangeEventHandler(e){
-      var rangeGroup = $(this).attr('name'),
-          minBtn = $(this).parent().children('.min'),
-          maxBtn = $(this).parent().children('.max'),
-          range_min = $(this).parent().children('.range_min'),
-          range_max = $(this).parent().children('.range_max'),
-          minVal = parseInt($(minBtn).val()),
-          maxVal = parseInt($(maxBtn).val()),
-          origin = $(this).context.className;
-
-      if(origin === 'min' && minVal > maxVal-5){
-          $(minBtn).val(maxVal-5);
-      }
-      var minVal = parseInt($(minBtn).val());
-      $(range_min).html(addSeparator(minVal*1000) + ' €');
+let min=document.querySelector(".min")
+let max=document.querySelector(".max")
+let range_min=document.querySelector(".range_min")
+let range_max=document.querySelector(".range_max")
 
 
-      if(origin === 'max' && maxVal-5 < minVal){
-          $(maxBtn).val(5+ minVal);
-      }
-      var maxVal = parseInt($(maxBtn).val());
-      $(range_max).html(addSeparator(maxVal*1000) + ' €');
-  }
 
-$('input[type="range"]').on( 'input', rangeInputChangeEventHandler);
-})();
 
+min.addEventListener("change",function(e){
+let val=e.target.value;
+range_min.innerText=val
+
+})
+max.addEventListener("change",function(e){
+  let val=e.target.value;
+range_max.innerText=val
+  })
+  
